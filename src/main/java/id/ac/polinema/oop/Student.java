f9955539a15000220b04ce377dc15f51219ce6aa
eldezaki <152847114+eldezaki@users.noreply.github.com>
2026-09-21 00:08:52 +0000

    gg

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -53,12 +53,12 @@ public class Student {
      */
     public String getPredicate() {
         // TODO: implement the predicate logic per the rules above
-        if (this.gpa >= 3.5) {
-            return "Cum Laude";
-        } else if (this.gpa >= 3.0) {
-            return "Very Satisfactory";
-        } else {
-            return "Satisfactory";
-        }
+        // if (this.gpa >= 3.5) {
+        //     return "Cum Laude";
+        // } else if (this.gpa >= 3.0) {
+        //     return "Very Satisfactory";
+        // } else {
+        //     return "Satisfactory";
+        // }
     }
 }
